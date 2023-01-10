a1d8e81aa8c87aa94835bac46eb7b9a97c86f909
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 19:56:29 +0530

    Added : rint the duplicate elements of an array

--- src/com/bridgelabz/problemsonarray/DuplicateElements.java
+++ src/com/bridgelabz/problemsonarray/DuplicateElements.java
@@ -1,2 +1,15 @@
-package com.bridgelabz.problemsonarray;public class DuplicateElements {
+package com.bridgelabz.problemsonarray;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 2, 4, 4, 3, 5, 5, 6, 7, 7, 8};
+        System.out.println("Duplicate Numbers in the Arrays are : ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[j]);
+                }
+            }
+        }
+    }
 }
