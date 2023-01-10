3f95145233abc6c550c31e0f598cbe07b4301835
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 20:11:18 +0530

    Added : Smallest element in an array

--- src/com/bridgelabz/problemsonarray/SmallestNumber.java
+++ src/com/bridgelabz/problemsonarray/SmallestNumber.java
@@ -1,2 +1,13 @@
-package com.bridgelabz.problemsonarray;public class SmallestNumber {
+package com.bridgelabz.problemsonarray;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int [] arr = new int [] {9, 1, 7, 8, 6};
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] < min)
+                min = arr[i];
+        }
+        System.out.println("Smallest Element in array: " + min);
+    }
 }
