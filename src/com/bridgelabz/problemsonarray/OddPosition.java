4bc1397921fa5398ed4d414822ac1cee215af29a
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 20:14:46 +0530

    Added : Elements of an array present on an odd position

--- src/com/bridgelabz/problemsonarray/OddPosition.java
+++ src/com/bridgelabz/problemsonarray/OddPosition.java
@@ -1,2 +1,11 @@
-package com.bridgelabz.problemsonarray;public class OddPosition {
+package com.bridgelabz.problemsonarray;
+
+public class OddPosition {
+    public static void main(String[] args) {
+        int[] ar = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Elements in Odd Position : ");
+        for (int i = 0; i < ar.length; i = i + 2) {
+            System.out.println(ar[i]);
+        }
+    }
 }
