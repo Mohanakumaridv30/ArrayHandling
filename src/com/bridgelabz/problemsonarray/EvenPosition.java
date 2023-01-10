0fc48723e7f54ad28aed26014bdff4c016e25d80
Mohanakumaridv30 <mohanakumaridv30@gmail.com>
2023-01-10 20:14:08 +0530

    Added : Elements of an array present on an even position

--- src/com/bridgelabz/problemsonarray/EvenPosition.java
+++ src/com/bridgelabz/problemsonarray/EvenPosition.java
@@ -1,2 +1,11 @@
-package com.bridgelabz.problemsonarray;public class EvenPosition {
+package com.bridgelabz.problemsonarray;
+
+public class EvenPosition {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 6, 9, 11, 50, 63};
+        System.out.println("Elements on array present on even position: ");
+        for (int i = 1; i < arr.length; i = i + 2) {
+            System.out.println(arr[i]);
+        }
+    }
 }
